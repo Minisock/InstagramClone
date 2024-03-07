4baec7bcc4fac7ab85e4db6164599e8570f4b295
Minisock <goldenkitamar@gmail.com>
2024-03-07 21:18:37 +0530

    First Commit
    Added Main Tab View

---
+++ InstagramClone/App/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Amarjeet Kumar on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
