8f76b1e3f3b9ec46cbad8efb33360c1473c35d4f
Minisock <goldenkitamar@gmail.com>
2024-03-09 21:37:01 +0530

    Done with Login UI

---
+++ InstagramClone/Core/Authentication/LoginView.swift
@@ -0,0 +1,120 @@
+//
+//  LoginView.swift
+//  InstagramClone
+//
+//  Created by Amarjeet Kumar on 09/03/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+     
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                // logo image
+                Image("instagram-logo-black")
+                    .resizable()
+                    .scaledToFit ()
+                    .frame(width: 220,height: 100)
+                
+                //text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Password", text: $password) //SecureField: gives password filling capabilities, recognises it as a password field, also asks the user if they wanna load a saved password from the key-chain and all the good stuff.
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                // forgot password button
+                Button {
+                    print("Show forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                // login button
+                Button {
+                    print("Login")
+                } label: {
+                    Text("Log in")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(10)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2)-40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2)-40, height: 0.5)
+                }
+                .foregroundColor(.gray)
+                
+                HStack {
+                    Image("facebook-logo")
+                        .resizable()
+                        .frame(width: 35, height: 20)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign Up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                } label: {
+                    HStack (spacing: 3){
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
